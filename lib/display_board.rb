a14ed8df4855c635255e38b39dd5ca0d777f45ad
Marvin Bonds <mbonds1@mail.depaul.edu>
2018-04-20 12:49:45 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,12 +3,12 @@ def display_board
   a = ["   ","   ","   ",
       "   ","   ","   ",
       "   ","   ","   "]
-  line = -----------
+  line = "-----------"
   
-  print " #{a[0]} | #{a[1]} | #{a[2]} "
-  print line
-  print " #{a[4]} | #{a[5]} | #{a[6]} "
-  print line
+  p " #{a[0]} | #{a[1]} | #{a[2]} "
+  p line
+  p " #{a[4]} | #{a[5]} | #{a[6]} "
+  p line
   p " #{a[7]} | #{a[8]} | #{a[9]} "
 end
 
