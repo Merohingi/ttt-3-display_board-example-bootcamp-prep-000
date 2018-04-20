28ad5a6dd66219d7e53f9b94336dbb98df825b6a
Marvin Bonds <mbonds1@mail.depaul.edu>
2018-04-20 12:13:46 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,3 +1,16 @@
+
+
 def display_board
+  Board = ["   ","|","   ","|","   "]
+  line = -----------
+  a = Board.new
+  b = Board.new
+  c = Board.new
+  puts a
+  puts line
+  puts b
+  puts line
+  puts c
+end
   
 
