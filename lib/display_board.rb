2526147609bee24065f838b2b231d507b92b1033
Marvin Bonds <mbonds1@mail.depaul.edu>
2018-04-20 12:30:23 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,14 +3,16 @@ Board = ["   ","   ","   "]
 def display_board
  
   line = -----------
-  a = board
-  b = board
-  c = board
-  puts a[0] |
-  puts line
-  puts b
-  puts line
-  puts c
+  a = Board.new
+  b = Board.new
+  c = Board.new
+  
+  puts " #{a[0]} | #{a[1]} | #{a[2]} "
+  puts "-----------"
+  puts " #{b[0]} | #{b[1]} | #{b[2]} "
+  puts "-----------"
+  puts " #{c[0]} | #{c[1]} | #{c[2]} "
+end
 end
   
 
