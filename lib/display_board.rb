c7b8ee24ba62ee6565c818e3b9d14c5ad72a0a54
Marvin Bonds <mbonds1@mail.depaul.edu>
2018-04-20 12:49:20 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,8 +7,8 @@ def display_board
   
   print " #{a[0]} | #{a[1]} | #{a[2]} "
   print line
-  p " #{a[4]} | #{a[5]} | #{a[6]} "
-  p line
+  print " #{a[4]} | #{a[5]} | #{a[6]} "
+  print line
   p " #{a[7]} | #{a[8]} | #{a[9]} "
 end
 
