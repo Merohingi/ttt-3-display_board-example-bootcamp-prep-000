3afbf9ea3e2f0637f103a2afbda6783f80b809bf
Marvin Bonds <mbonds1@mail.depaul.edu>
2018-04-20 13:11:14 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,11 +3,11 @@ def display_board
 board = Array.new(9, " ")
   line = "-----------"
   
-  puts "#{board[0]} | #{a[1]} | #{a[2]}"
+  puts "#{board[0]} | #{board[1]} | #{board[2]}"
   puts line
-  puts "#{board[3]} | #{a[4]} | #{a[5]}"
+  puts "#{board[3]} | #{board[4]} | #{board[5]}"
   puts line
-  puts "#{a[6]} | #{a[7]} | #{a[8]}"
+  puts "#{board[6]} | #{board[7]} | #{board[8]}"
 end
 
   
