3aa54ac063e1463fc338ea7348bc3d17b8031498
Marvin Bonds <mbonds1@mail.depaul.edu>
2018-04-20 13:18:44 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,16 +1,8 @@
-
 def display_board
-  
-  board = ["   " "|" "   " "|" "   ", "   " "|" "   " "|" "   ", "   " "|" "   " "|" "   "]
-
-  line = "-----------"
-  
-  puts "#{board[0]} | #{board[1]} | #{board[2]}"
-  puts line
-  puts "#{board[3]} | #{board[4]} | #{board[5]}"
-  puts line
-  puts "#{board[6]} | #{board[7]} | #{board[8]}"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 end
-
-  
 
